2bba215782fa8c3e2098e72bd4fcb22dad0c0458
Doug <karaballo.douglas@hotmail.com>
2021-11-24 13:13:10 -0400

    Añadiendo los estilos de la seccion destacados

--- template-parts/components/home/element-featured.php
+++ template-parts/components/home/element-featured.php
@@ -20,11 +20,21 @@
             while($query_featured -> have_posts()):
                 $query_featured-> the_post();
         ?>
-        <article>
-            <figure>
+        <article class="FeaturedPost">
+            <figure class="FeaturedFigure">
                 <a href="<?= esc_url(get_permalink($query_featured->id));?>">
-                    <?php the_post_thumbnail( '', ['class'=>'FeaturedImg']) ?>
+                    <?php the_post_thumbnail( '', ['class'=>'FeaturedImg']); ?>
                 </a>
             </figure>
+            <div class="FeaturedInfo">
+                <span class="FeatureInfoDate"><h5><?php the_author();?> / <?php echo get_the_date('M d,Y');?></h5></span>
+                <?php the_title( '<h3 class="FeaturedTitle"><a href='.get_the_permalink().'>','</a></h3>'); ?>
+                <?php the_excerpt(); ?>
+            </div>
+            <div class="SeeMore">
+                <a href="<?= esc_url(get_permalink($query_featured->id));?>">Ver post</a>
+            </div>
         </article>
+        <?php endwhile; ?>
+    <?php endif;?>
 </div>
---
+++ template-parts/content-home.php
@@ -0,0 +1,5 @@
+<div class="HomePage">
+    <div class="HomePost">
+        <?php get_template_part('template-parts/components/home/element','featured'); ?>
+    </div>
+</div>
