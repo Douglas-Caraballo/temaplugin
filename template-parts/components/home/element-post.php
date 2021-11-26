fe488be4fdd233e733c6ccc6f2451eb279aaaeee
Doug <karaballo.douglas@hotmail.com>
2021-11-26 11:09:00 -0400

    CReacion de la seccion post reciente y agregando estilos a la seccion

---
+++ template-parts/components/home/element-post.php
@@ -0,0 +1,41 @@
+<?php
+
+    $args = [
+        'post_type' => 'post',
+        'post_status' => 'publish',
+        'order'=>'DESC',
+        'paged' => 1,
+        'posts_per_page' => 4,
+    ];
+
+    $blog = new WP_Query($args);
+?>
+<div class="WrapperPosts">
+    <span class="SectionTitle"><h1>Post Recientes</h1></span>
+    <div class="ContentPost">
+        <?php if($blog -> have_posts()):?>
+            <?php
+                while($blog -> have_posts()):
+                    $blog -> the_post();
+            ?>
+            <article class="ContenPostItem">
+                    <figure class="PostImg">
+                        <a href="<?= esc_url(get_permalink($blog->id)); ?>">
+                            <?php the_post_thumbnail( '', ['class'=>'BlogImg']);?>
+                        </a>
+                    </figure>
+                    <div>
+                        <span class="PostCategories"><h5 class="PostCategoriesName">- <?php the_category(' / ');?> -</h5></span>
+                        <?php the_title( '<h3 class="FeaturedTitle"><a href='.get_the_permalink().'>','</a></h3>'); ?>
+                    </div>
+                    <div class="PostExcerpt">
+                        <?php the_excerpt();?>
+                    </div>
+                    <div>
+                        <span class="FeatureInfoDate"><h5><?php the_author();?> / <?php echo get_the_date('M d,Y');?></h5></span>
+                    </div>
+            </article>
+            <?php endwhile; ?>
+        <?php endif; ?>
+    </div>
+</div>
--- template-parts/content-home.php
+++ template-parts/content-home.php
@@ -1,6 +1,7 @@
 <div class="HomePage">
     <div class="HomePost">
         <?php get_template_part('template-parts/components/home/element','featured'); ?>
+        <?php get_template_part('template-parts/components/home/element','post'); ?>
     </div>
     <?php get_template_part('template-parts/components/home/element','widgets'); ?>
 </div>
