318091a9f8ad7f1d4ea7b74fa4a7654e9f3e07f4
Doug <karaballo.douglas@hotmail.com>
2021-11-26 14:11:08 -0400

    Modificaciones a los estilos del area de widgets del home y a los estilos de la seccion de post recientes

--- template-parts/components/home/element-widgets.php
+++ template-parts/components/home/element-widgets.php
@@ -1,5 +1,7 @@
+<div class="WrapperHomeWidgets">
 <?php if(is_active_sidebar('home')): ?>
     <aside id="home" class="HomeWidgetsArea">
         <?php dynamic_sidebar('home'); ?>
     </aside>
 <?php endif; ?>
+</div>
