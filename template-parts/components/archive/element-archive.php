2b7f72006c36044c2be4cb1769e260b30c4df8f3
Doug <karaballo.douglas@hotmail.com>
2021-12-08 11:33:40 -0400

    Modificacion al area de autor en el archive

--- template-parts/components/archive/element-archive.php
+++ template-parts/components/archive/element-archive.php
@@ -7,7 +7,7 @@
     <div class="ArchiveItemDate">
         <span><?php the_title('<h3 class="ArchiveTitlePost"><a href='.get_the_permalink().'>','</a></h3>'); ?></span>
         <span><?php the_excerpt(); ?></span>
-        <span><?php the_author(); ?></span>
+        <span class="ArchivePostAutor"><?php the_author(); ?></span>
         <a class="SeeMore" href="<?= esc_url(get_permalink()); ?>">Leer</a>
     </div>
 </article>
