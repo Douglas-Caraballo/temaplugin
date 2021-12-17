8cf7f64672e2d27f40daf28db12fe80123abb1cb
Doug <karaballo.douglas@hotmail.com>
2021-12-17 09:27:39 -0400

    Modificaciones a los estilos de la single page

--- template-parts/components/single/element-single.php
+++ template-parts/components/single/element-single.php
@@ -2,9 +2,9 @@
     <div class="SingleHeader">
         <?php the_title('<h1 class="SingleTitle">','</h1>'); ?>
         <div class="SingleHeaderInfo">
-            <span><h5><?php the_author();?></h5></span>
-            <span><h5><?php echo get_the_date('M d, Y'); ?></h5></span>
-            <span><h5><a href="<?=comments_link();?>"><?php echo get_comments_number(get_the_ID( ));?></a></h5></span>
+            <span class="SingleAutor SinglePostInfo"><h5><?php the_author();?></h5></span>
+            <span class="SingleDate SinglePostInfo"><h5><?php echo get_the_date('M d, Y'); ?></h5></span>
+            <span class="SingleComments SinglePostInfo"><h5><a href="<?=comments_link();?>"><?php echo get_comments_number(get_the_ID( ));?></a></h5></span>
         </div>
     </div>
     <div class="SingleContent">
