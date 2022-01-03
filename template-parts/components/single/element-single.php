0fb57a950194345ad0ad54967b3714288261c511
Doug <karaballo.douglas@hotmail.com>
2022-01-03 13:06:14 -0400

    Cambios a los estilos de la single page (autor y bloque guntenberg) y agregada la funcionalidad de ver todos los post de un autor

--- template-parts/components/single/element-single.php
+++ template-parts/components/single/element-single.php
@@ -2,7 +2,10 @@
     <div class="SingleHeader">
         <?php the_title('<h1 class="SingleTitle">','</h1>'); ?>
         <div class="SingleHeaderInfo">
-            <span class="SingleAutor SinglePostInfo"><h5><?php the_author();?></h5></span>
+            <span class="SingleAutor SinglePostInfo"><h5>
+                <a href="<?php echo esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ); ?>">
+                    <?php the_author();?>
+                </a></h5></span>
             <span class="SingleDate SinglePostInfo"><h5><?php echo get_the_date('M d, Y'); ?></h5></span>
             <span class="SingleComments SinglePostInfo"><h5><a href="<?=comments_link();?>"><?php echo get_comments_number(get_the_ID( ));?></a></h5></span>
         </div>
